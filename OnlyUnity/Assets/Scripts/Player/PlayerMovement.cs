6224a4abecc706f6bd67bcff9c029e2711cc0fb6
Darelund <edlund.vidar@gmail.com>
2024-03-26 16:27:41 +0100

    Player Input

--- OnlyUnity/Assets/Scripts/Player/PlayerMovement.cs
+++ OnlyUnity/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float Speed;
-
+    private Vector2 _movementVector;
+    private Rigidbody _rB;
     void Start()
     {
-        
+        _rB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _rB.AddForce(_movementVector.x * Speed, 0.0f, _movementVector.y * Speed);
     }
-    void OnMove()
+    void OnMove(InputValue movementValue)
     {
-
+        _movementVector = movementValue.Get<Vector2>();
     }
 }
