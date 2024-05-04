ffc74efae9fd73da85eb25f2c9e1803583e143b1
Darelund <edlund.vidar@gmail.com>
2024-05-05 00:42:40 +0200

    Added some small fixes
    Ok, I Fixed* things

--- OnlyUnity/Assets/Scripts/Player/PlayerMouseLook.cs
+++ OnlyUnity/Assets/Scripts/Player/PlayerMouseLook.cs
@@ -8,7 +8,8 @@ public class PlayerMouseLook : MonoBehaviour
     public float mouseSensitivity = 100f;
     public Transform player;
     private Vector2 _mouseVector;
-    private float xRotation = 0f;
+    //private float xRotation = 0f;
+    private float yRotation = 0f;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -21,9 +22,11 @@ public class PlayerMouseLook : MonoBehaviour
         Cursor.visible = false;
         float mouseX = _mouseVector.x * mouseSensitivity * Time.deltaTime;
         float mouseY = _mouseVector.y * mouseSensitivity * Time.deltaTime;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        //xRotation -= mouseY;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //yRotation -= mouseX;
+        yRotation = Mathf.Clamp(-mouseY, -45f, 45f);
+        transform.Rotate(yRotation, 0f, 0f);
         player.Rotate(Vector3.up * mouseX);
     }
     void OnLook(InputValue MouseValue)
